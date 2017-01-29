194e30141c463cd52b9bec017cbbf9e324d1d9a5
Christopher Warner <chrism339@yahoo.com>
2017-01-29 13:02:52 -0600

    Add files via upload

---
+++ HellOnBlock/src/main/java/me/Cleardragonf/HOB/BonusSpawning.java
@@ -0,0 +1,94 @@
+package me.Cleardragonf.HOB;
+
+import java.util.Random;
+
+import org.spongepowered.api.entity.Entity;
+import org.spongepowered.api.entity.EntityTypes;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.entity.SpawnEntityEvent;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
+
+public class BonusSpawning {
+	  @Listener
+	  public void checkEntitySpawn(SpawnEntityEvent event){
+	  	  //This is the testing code v
+	  	  Entity spawnedEntity = null;
+	  	  for(Entity entity : event.getEntities()){//
+	  		  if(entity != null){
+	  			  spawnedEntity = entity;
+	  			  break;
+	  		  }
+	  	  }
+	  	  if(spawnedEntity == null){
+	  		  return;
+	  	  }
+	  	  if(spawnedEntity.getType().equals(EntityTypes.ENDER_DRAGON)){
+	  		  return;
+	  	  }  		 
+	  	  else if(spawnedEntity.getType().equals(EntityTypes.GHAST)){
+	  		  return;
+	  	  }
+	  	  else if(spawnedEntity.getType().equals(EntityTypes.BLAZE)){
+	  		  return;
+	  	  }
+	  	  else if(spawnedEntity.getType().equals(EntityTypes.CHESTED_MINECART)){
+	  		  return;
+	  	  }
+	  	  else if(spawnedEntity.getType().equals(EntityTypes.FALLING_BLOCK)){
+	  		  return;
+	  	  }
+	  	  else if(spawnedEntity.getType().equals(EntityTypes.ITEM)){
+	  		  return;
+	  	  }
+	  	  else if(spawnedEntity.getType().equals(EntityTypes.UNKNOWN)){
+	  		  return;
+	  	  }
+	  	  else if(spawnedEntity.getType().equals(EntityTypes.GIANT)){
+	  		  return;
+	  	  }else{
+	  		  ConfigurationManager.getInstance();
+			if(ConfigurationManager.getInstance().getConfig().getNode("Spawning Bonuses!", "Ender Dragon", "Enabled").getString().equals("True")){
+	  			  for (int i = 0; i < (ConfigurationManager.getInstance().getConfig().getNode("Spawning Bonuses!", "Ender Dragon", "#").getInt()); i++){
+	  				  Random roll = new Random();
+	  				  int answer = roll.nextInt(100)+1;
+	  				  if (answer <= (ConfigurationManager.getInstance().getConfig().getNode("Spawning Bonuses!", "Ender Dragon", "%").getInt())){
+	  					  Location<World> location = spawnedEntity.getLocation();
+	  					  World world = location.getExtent();
+	  					  Entity newEntity = world.createEntity(EntityTypes.ENDER_DRAGON, location.getPosition());
+	  					  	event.getEntities().add(newEntity);
+	  				  }
+	  			  }
+	  		  } else {
+				ConfigurationManager.getInstance();
+				if(ConfigurationManager.getInstance().getConfig().getNode("Spawning Bonuses!", "Ghast", "Enabled").getString().equals("True")){
+					  for (int i = 0; i < (ConfigurationManager.getInstance().getConfig().getNode("Spawning Bonuses!", "Ghast", "#").getInt()); i++){
+						  Random roll = new Random();
+						  int answer = roll.nextInt(100)+1;
+						  if (answer <= (ConfigurationManager.getInstance().getConfig().getNode("Spawning Bonuses!", "Ghast", "%").getInt())){
+							  Location<World> location = spawnedEntity.getLocation();
+							  World world = location.getExtent();
+							  Entity newEntity = world.createEntity(EntityTypes.GHAST, location.getPosition());
+							  	event.getEntities().add(newEntity);
+						  }
+					  }
+				  } else {
+					ConfigurationManager.getInstance();
+					if(ConfigurationManager.getInstance().getConfig().getNode("Spawning Bonuses!", "Giant", "Enabled").getString().equals("True")){
+						  for (int i = 0; i < (ConfigurationManager.getInstance().getConfig().getNode("Spawning Bonuses!", "Giant", "#").getInt()); i++){
+							  Random roll = new Random();
+							  int answer = roll.nextInt(100)+1;
+							  if (answer <= (ConfigurationManager.getInstance().getConfig().getNode("Spawning Bonuses!", "Giant", "%").getInt())){
+								  Location<World> location = spawnedEntity.getLocation();
+								  World world = location.getExtent();
+								  Entity newEntity = world.createEntity(EntityTypes.GIANT, location.getPosition());
+								  	event.getEntities().add(newEntity);
+							  }
+						  }
+					  }
+				}
+			}
+	  		  
+	  	  }
+	    }
+}
